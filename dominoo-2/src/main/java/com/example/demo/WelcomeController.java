fe0af73d0de8264d50adb5a0c7dfc748c70bec18
moonYongEun <hafg7751@gmail.com>
2019-06-25 02:48:05 +0900

    mybatis연결 중 내일 시도06 25

--- dominoo-2/src/main/java/com/example/demo/WelcomeController.java
+++ dominoo-2/src/main/java/com/example/demo/WelcomeController.java
@@ -1,15 +1,34 @@
 package com.example.demo;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import index.dao.indexDAO;
 
 @Controller
 public class WelcomeController {
+@Autowired
+	private indexDAO indexdao;
 	
 	@RequestMapping("/index")
 	public String index() {
-		System.out.println("여기로 왓니");
 		return "helloworld";
 	}
+	
+	@RequestMapping("/insert")
+	public String insert() {
+		return "insert";
+	}
+	
+	@RequestMapping("/insert2")
+	public String insert2(@RequestParam Map<String,String>map) {
+		
+		return "index";
+	}
+	
 
 }
--- dominoo-2/src/main/webapp/WEB-INF/index/helloworld.jsp
+++ dominoo-2/src/main/webapp/WEB-INF/index/helloworld.jsp
@@ -3,10 +3,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+<style type="text/css">
+li{cursor:pointer;}
+</style>
+
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
-  바꼈다 ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ 굿!
+ <h2>입력 상자</h2> 
+<ul>
+	<li id="insert">1.입력</li>
+	<br>
+	<li>2.보기</li>
+	<br>
+	<li>3.수정</li>
+	<br>
+	<li>4.삭제</li>
+</ul> 
 </body>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script>
+$('#insert').click(function(){
+	location.href="/insert";
+	
+});
+</script>
 </html>
